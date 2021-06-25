362c29302591ce3b474f7c22634f6faf228a0c04
Alan Russell <alan.russell@digirati.com>
2021-06-25 16:33:27 +0100

    Add high-level tests
    Skip failing test cases

---
+++ src/parse.test.ts
@@ -0,0 +1,26 @@
+import { parse } from './parse';
+
+// Given the current date and time is 2020-05-01T00:00:00:
+jest.useFakeTimers('modern').setSystemTime(new Date('2020-05-01T00:00:00Z').getTime());
+
+describe.skip('parse', () => {
+  test('now minus one year rounded to the nearest year', () => {
+    expect(parse('now-1y/y')).toEqual(new Date('2019-01-01T00:00:00'));
+  });
+
+  test('now rounded to the nearest year', () => {
+    expect(parse('now/y')).toEqual(new Date('2020-01-01T00:00:00'));
+  });
+
+  test('now minus 1 day', () => {
+    expect(parse('now-1d')).toEqual(new Date('2020-04-30T00:00:00'));
+  });
+
+  test('now add 1 day', () => {
+    expect(parse('now+1d')).toEqual(new Date('2020-05-02T00:00:00'));
+  });
+
+  test('now minus four days and four hours', () => {
+    expect(parse('now-4d-4h')).toEqual(new Date('2020-04-26T20:00:00'));
+  });
+});
---
+++ src/parse.ts
@@ -0,0 +1,4 @@
+export function parse(datestring: DateString): Date {
+  datestring.toString();
+  return new Date();
+}
